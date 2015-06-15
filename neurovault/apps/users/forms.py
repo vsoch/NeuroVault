c7c25580c73e926ffce71d0fb5a99e070cb2d4ab
vsoch <vsochat@stanford.edu>
2015-06-15 11:51:07 -0700

    create example of pop up signup form

--- neurovault/apps/users/forms.py
+++ neurovault/apps/users/forms.py
@@ -1,7 +1,8 @@
-from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm,\
     PasswordChangeForm
+from django.contrib.auth.models import User
+from crispy_forms.helper import FormHelper
+from django import forms
 
 class UserCreateForm(UserCreationForm):
     email = forms.EmailField(required=True)
